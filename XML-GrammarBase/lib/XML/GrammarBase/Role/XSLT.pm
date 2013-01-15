a93d74c4247c37c4a7c275363ecb7e232a958a5e
Shlomi Fish <shlomif@shlomifish.org>
2013-01-15 14:10:12 +0200

    New version of XML::GrammarBase.

--- XML-GrammarBase/lib/XML/GrammarBase/Role/XSLT.pm
+++ XML-GrammarBase/lib/XML/GrammarBase/Role/XSLT.pm
@@ -10,7 +10,7 @@ XML::GrammarBase::Role::XSLT - a parameterized role for XSLT conversions.
 
 =head1 VERSION
 
-Version 0.1.2
+Version 0.1.3
 
 =cut
 
@@ -25,7 +25,7 @@ use XML::LibXSLT;
 
 use autodie;
 
-our $VERSION = '0.1.2';
+our $VERSION = '0.1.3';
 
 
 sub make_variant
