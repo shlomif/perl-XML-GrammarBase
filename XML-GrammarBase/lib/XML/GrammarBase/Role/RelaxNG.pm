a5d7078d2df5c77cd7a5ea90f003ce08692b9d55
Shlomi Fish <shlomif@shlomifish.org>
2013-01-18 23:03:29 +0200

    Bump version to 0.1.5.

--- XML-GrammarBase/lib/XML/GrammarBase/Role/RelaxNG.pm
+++ XML-GrammarBase/lib/XML/GrammarBase/Role/RelaxNG.pm
@@ -10,7 +10,7 @@ XML::GrammarBase::Role::RelaxNG - base class for a RelaxNG validator
 
 =head1 VERSION
 
-Version 0.1.4
+Version 0.1.5
 
 =cut
 
@@ -21,7 +21,7 @@ use XML::LibXML;
 
 with ('XML::GrammarBase::Role::DataDir');
 
-our $VERSION = '0.1.4';
+our $VERSION = '0.1.5';
 
 has 'rng_schema_basename' => (isa => 'Str', is => 'rw');
 has '_rng' =>
